f785db035d45a3e80f1f209ef2c4ac7a0b4fe7ce
Kateryna Osmachko <kateryna.y@ukr.net>
2016-03-22 21:12:03 +0200

    scan dir

---
+++ functions_forms_tasks/5.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Написать функцию, которая выводит список файлов в заданной директории, которые содержат искомое слово.
+Директория и искомое слово задаются как параметры функции.*/
+
+function filesInDir($dir ,$fileName) {
+    if (is_dir($dir)) {
+        if ($handle = opendir($dir)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != ".." && !is_dir($file) && (strstr($file, $fileName) == true)) {
+                    echo "$file <br> ";
+                }
+            }
+            closedir($handle);
+        }
+    } else die("Вы не задали параметр функции ДИРЕКТОРИЯ!");
+}
+filesInDir('c:/', "fi");
