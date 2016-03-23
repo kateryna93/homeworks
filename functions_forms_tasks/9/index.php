411c513c5104d18bd608e6ba3a7e1dede8c66260
Kateryna Osmachko <kateryna.y@ukr.net>
2016-03-23 10:10:48 +0200

    reverse string

---
+++ functions_forms_tasks/9/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Написать функцию, которая переворачивает строку. Было "abcde", должна выдать "edcba". Ввод текста реализовать с помощью формы.
+
+if ($_POST["string"]) {
+    $string = $_POST["string"];
+}
+else {
+    die("Заполните поле формы!");
+}
+function reverseString($string) {
+    $count = strlen($string);
+    $emptyString = '';
+    for ($i = $count - 1; $i >= 0; $i--) { //переворачиваем ключи в обратном напрвлении
+        $emptyString .= $string[$i]; //за допомогою конкатенації клеїмо символи у зворотному порядку
+    }
+    return $emptyString;
+}
+echo reverseString($string);
+
+
+include 'reverse_string.html';
